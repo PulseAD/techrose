14fc7cf4dcf0948485f4a7e7c04be727e7297ce7
Antoine-webdigit <a.derouck@webdigit.be>
2020-09-05 14:32:09 +0200

    Adding mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,44 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import "./layout.css"
 import menuSvg from '../images/menu.svg'
+import closeMenuSvg from '../images/close.svg'
+import { Link } from "gatsby"
 
 const Header = (props) => {
+  const [isMenuShown, setIsMenuShown] = useState(false);
   const color = props.color ? props.color : '#000';
   const hamburgerMenuColor = props.color == '#fff' ? 'invert(1)' : 'none';
   return (
-    <header class="header">
-      <h3 class="header__logo" style={{color: color}}>TechRose</h3>
-      <div class="header__menu">
-        <img src={menuSvg} alt="menu hamburger" style={{filter: hamburgerMenuColor}}/>
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <h3 className="header__logo" style={{color: color}}>TechRose</h3>
+        <div className="header__menu">
+          <img
+            src={menuSvg}
+            alt="menu hamburger"
+            style={{filter: hamburgerMenuColor}}
+            onClick={() => setIsMenuShown(true)}
+          />
+        </div>
+      </header>
+      {isMenuShown &&
+        <nav className="fullscreen-menu">
+          <div className="fullscreen-menu__header">
+            <h3 className="fullscreen-menu__header__logo">TechRose</h3>
+            <div className="fullscreen-menu__header__close">
+            <img src={closeMenuSvg} alt="close menu" onClick={() => setIsMenuShown(false)} />
+            </div>
+          </div>
+          <ul>
+            <li><Link to="/" onClick={() => setIsMenuShown(false)}>Home</Link></li>
+            <li><Link to="/services" onClick={() => setIsMenuShown(false)}>Our services</Link></li>
+            <li><Link to="/portfolio" onClick={() => setIsMenuShown(false)}>Portfolio</Link></li>
+            <li><Link to="/contact" onClick={() => setIsMenuShown(false)}>Contact</Link></li>
+          </ul>
+        </nav>
+      }
+    </>
   )
 }
 
