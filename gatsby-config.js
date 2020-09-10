ad44f44c208755c482668f7f43f535153f9ebb46
Antoine-webdigit <a.derouck@webdigit.be>
2020-09-10 20:22:14 +0200

    Trying fonts locally

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,14 +27,6 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        google: {
-          families: ['Poppins:300,600', 'Playfair Display:700']
-        }
-      }
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
